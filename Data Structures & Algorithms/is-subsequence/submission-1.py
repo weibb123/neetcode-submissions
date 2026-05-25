53aeb0bcec50f5ed33dd1688c73597fd148e8a0d
Weining Mai <84426364+weibb123@users.noreply.github.com>
2026-05-25 01:28:31 -0400

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # Have 2 pointers track the 2 strings
+        # if s[i] == s[j] advance pointers
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            # j advances
+            j += 1
+        return i == len(s) # went through all of string s
+        
